77265400388dbde117c0f840b8cc8c70bf4f3288
HannahEleenN <164185818+HannahEleenN@users.noreply.github.com>
2026-01-29 16:38:49 +0100

    Update client_app, ToS and privacy

---
+++ client/client_app.js
@@ -0,0 +1,23 @@
+import { privacyPolicy, termsOfService } from './modules/legal_content.js';
+
+// Elements
+const consentBox = document.getElementById('consent-checkbox');
+const registerBtn = document.getElementById('register-btn');
+const modal = document.getElementById('legal-modal');
+const modalText = document.getElementById('legal-text');
+
+// Activate button only if checkbox is checked
+consentBox.addEventListener('change', () => {
+    registerBtn.disabled = !consentBox.checked;
+});
+
+// Show ToS/Privacy when clicked
+document.getElementById('view-tos').onclick = () => {
+    modalText.innerHTML = termsOfService;
+    modal.style.display = 'block';
+};
+
+document.getElementById('view-privacy').onclick = () => {
+    modalText.innerHTML = privacyPolicy;
+    modal.style.display = 'block';
+};
