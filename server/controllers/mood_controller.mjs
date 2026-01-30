438436de564e91cd1823d3a7e58745dbadc58831
Hannah <164185818+HannahEleenN@users.noreply.github.com>
2026-01-30 15:02:47 +0100

    Extract date formatting to formatDate function
    Refactor date formatting to a separate function.

--- server/controllers/mood_controller.mjs
+++ server/controllers/mood_controller.mjs
@@ -1,22 +1,27 @@
-import { Mood } from '../models/mood_model.mjs';
+import Mood from '../models/mood_model.mjs';
+
+// Norwegian format
+function formatDate(date) 
+{
+    return date.toLocaleDateString('no-NO', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
+}
 
 export const createMoodEntry = (req, res) => 
   {
     const { parentId, childName, mood, context } = req.body;
 
-    // Create Norwegian date format here
-    const norDate = new Date().toLocaleDateString('no-NO', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric'
-    });
+    const dateForStorage = formateDate(new Date()); 
 
     const newEntry = Mood.create({
         parentId,
         childName,
         mood,
         context,
-        timestamp: norDate
+        timestamp: dateForStorage
     });
 
     res.status(201).json(newEntry);
