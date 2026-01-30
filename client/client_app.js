995faddcd0d6b3ab03fc27e629b2fce89d5e390c
HannahEleenN <164185818+HannahEleenN@users.noreply.github.com>
2026-01-30 15:21:25 +0100

    Updated client_app

--- client/client_app.js
+++ client/client_app.js
@@ -1,10 +1,29 @@
 import { privacyPolicy, termsOfService } from './modules/legal_content.js';
 
+const modal = document.getElementById('legal-modal');
+const modalText = document.getElementById('legal-text');
+
 // Elements
 const consentBox = document.getElementById('consent-checkbox');
 const registerBtn = document.getElementById('register-btn');
 const modal = document.getElementById('legal-modal');
 const modalText = document.getElementById('legal-text');
+
+// Open terms of service
+document.getElementById('view-tos').onclick = (e) =>
+{
+    e.preventDefault();
+    modalText.innerHTML = termsOfService;
+    modal.style.display = 'block';
+};
+
+// Open privacy policy
+document.getElementById('view-privacy').onclick = (e) =>
+{
+    e.preventDefault();
+    modalText.innerHTML = privacyPolicy;
+    modal.style.display = 'block';
+};
 
 // Activate button only if checkbox is checked
 consentBox.addEventListener('change', () => {
